87322db3daf9ed4511939d8db024ea0e9fe61175
Sanyog Sharma <sanyog96@gmail.com>
2017-08-15 22:54:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,12 +21,21 @@ var submit= document.getElementById('submit-btn');
 submit.onclick = function() {
     //should make a request to server and send the name
     //capture the list of name and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for(var i=0; i<names.length; i++)
-    {
-        list = list + "<li>"+ names[i] + "</li>";
-    }
-    var ul= document.getElementById('namelist');
-    ul.innerHTML = list;
+    var request = new XMLHttpRequest(); 
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var names = ['name1', 'name2', 'name3', 'name4'];
+                var list = '';
+                for(var i=0; i<names.length; i++)
+                {
+                list = list + "<li>"+ names[i] + "</li>";
+                }
+                var ul= document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    request.open('Get', "http://sanyog96.imad.hasura-app.io/submit-name?name="+name, true);
+    request.send(null);
 };
