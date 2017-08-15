285bd9ae565882f2310ceae90ce1816b3d48ffcd
Sanyog Sharma <sanyog96@gmail.com>
2017-08-15 21:55:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,13 +4,11 @@ button.onclick = function() {
     var request = new XMLHttpRequest(); 
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE)
-        {
             if(request.status === 200)
             {
                 var counter = request.responseText;
                 document.getElementById('count').innerHTML = counter.toString();
             }
-        }
     };
     request.open('Get', 'http://sanyog96.imad.hasura.app.io/counter', true);
     request.send(null);
