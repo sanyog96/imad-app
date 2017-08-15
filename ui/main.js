5c54073efaae08772c45c640686bb1728115e22e
Sanyog Sharma <sanyog96@gmail.com>
2017-08-15 21:56:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,6 @@ button.onclick = function() {
                 document.getElementById('count').innerHTML = counter.toString();
             }
     };
-    request.open('Get', 'http://sanyog96.imad.hasura.app.io/counter', true);
+    request.open('Get', "http://sanyog96.imad.hasura.app.io/counter", true);
     request.send(null);
 };
