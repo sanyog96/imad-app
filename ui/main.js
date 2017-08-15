8d34c331eb58dd7bc2a3ba9b4685af3f61e96bcb
Sanyog Sharma <sanyog96@gmail.com>
2017-08-15 21:16:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML = 'New Value';
 
 //move the image
 var img = document.getElementById('madi');
-var marginleft = 0;
+var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
