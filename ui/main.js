cfcd014471151377f3f30b19993b1853460df400
Sanyog Sharma <sanyog96@gmail.com>
2017-08-15 22:21:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,20 @@ button.onclick = function() {
     request.open('Get', "http://sanyog96.imad.hasura-app.io/counter", true);
     request.send(null);
 };
+
+//chnage name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick = function() {
+    //should make a request to server and send the name
+    //capture the list of name and render it as a list
+    var names = ['name1', 'name2', 'name3'];
+    var list = '';
+    for(var i=0;i<names.length; i++)
+    {
+        list += "<li>"+ name[i] + "</li>";
+    }
+    var ul= document.getElementById('namelist');
+    ul.innerHTML=list;
+};
