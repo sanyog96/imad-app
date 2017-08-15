e43e605158994c451c2b4a4ac736490953e52294
Sanyog Sharma <sanyog96@gmail.com>
2017-08-15 21:42:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,3 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main text');
-element.innerHTML = 'New Value';
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 100);//Every 100 millisecond apply  moveRight function
-};
-
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
