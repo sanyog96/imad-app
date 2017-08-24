39b8d9c6752e1e65a435d4a4289eb199b2d2dcc6
Sanyog Sharma <sanyog96@gmail.com>
2017-08-24 20:39:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var pool = require('pg').Pool;
 var crypto = require('crypto');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var config = {
     user: 'sanyog96',
     database: 'sanyog96',
